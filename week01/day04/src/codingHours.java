c46e5ab11fdce1adb6b139533f077ace08bbbf5b
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-02 14:04:07 +0200

    new java files

---
+++ week01/day04/src/codingHours.java
@@ -0,0 +1,14 @@
+public class codingHours {
+  public static void main(String[] args) {
+
+    int daily = 6;
+    int days = 17 * 5;
+
+    System.out.println(daily * days);
+
+    double average = 52.;
+    double percentage = (average / (daily * days)) *100;
+    System.out.println(percentage + " %");
+
+  }
+}
---
+++ week01/day04/src/favoriteNumber.java
@@ -0,0 +1,6 @@
+public class favoriteNumber {
+  public static void main(String[] args) {
+    int fav = 7;
+    System.out.println("My fav number is " + fav);
+  }
+}
