3b0e49f55a403100b07ba3937d30cbec27885a10
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-14 11:26:35 +0200

    Init WriteMultipleLines

---
+++ week03/day02/src/WriteMultipleLines.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+    List<String> content = new ArrayList();
+    content.add("Gyarmati Ádám");
+    Path getPath = Paths.get("my-file.txt");
+    try {
+      Files.write(getPath, content);
+      System.out.println("Ok, added content");
+    } catch (IOException ex) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
+  }
+}
