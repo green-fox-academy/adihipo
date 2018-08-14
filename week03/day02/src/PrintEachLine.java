cccdc8536a20807e63df84f2cfc7f42d3bba1abf
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-14 11:13:01 +0200

    Unable to read file added

--- week03/day02/src/PrintEachLine.java
+++ week03/day02/src/PrintEachLine.java
@@ -1,14 +1,19 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class PrintEachLine {
   public static void main(String[] args) {
-    // Write a program that opens a file called "my-file.txt", then prints
-    // each of the lines form the file.
-    // If the program is unable to read the file (for example it does not exists),
-    // then it should print an error message like: "Unable to read file: my-file.txt"
-
-
+    Path getPath = Paths.get("my-file.txt");
+    try {
+      List<String> lines = Files.readAllLines(getPath);
+      for (String line : lines) {
+        System.out.println(line);
+      }
+    } catch (IOException ex) {
+      System.out.println("Unable to read file: my-file.txt");
+    }
   }
 }
