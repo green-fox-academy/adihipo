d177a6dcb369e22b4a3ad7d78905daa23003e1c9
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-14 10:48:40 +0200

    DivideByZero code added

---
+++ week03/day02/src/DivideByZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Gimme a number. 10 will be divided by that. Tip: try 0!");
+    int divisor = scanner.nextInt();
+
+    try {
+      int result = 10 / divisor;
+      System.out.println(result);
+    } catch (Exception ex) {
+      System.out.println("fail");
+    }
+
+  }
+}
