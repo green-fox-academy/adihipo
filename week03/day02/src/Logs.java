5e1ca51abe99aa445f251f3412ad975e8b363018
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-14 16:13:43 +0200

    Logs purrfect

--- week03/day02/src/Logs.java
+++ week03/day02/src/Logs.java
@@ -11,9 +11,8 @@ public class Logs {
     Path getPath = Paths.get(file);
     try {
       List<String> lines = Files.readAllLines(getPath);
-
       System.out.println(uniqIP(lines));
-      //System.out.println(ratioGetPost(lines));
+      System.out.println(ratioGetPost(lines));
     } catch (IOException ex) {
       System.out.println("Unable to read file: " + file);
     }
@@ -30,8 +29,20 @@ public class Logs {
     return IPs;
   }
 
-//  public static double ratioGetPost(String file) {
-//    double ratio;
-//    return ratio;
-//  }
+  public static double ratioGetPost(List<String> lines) {
+    double GET = 0;
+    double POST = 0;
+    ArrayList<String> GETPOST = new ArrayList<>();
+    for (int i = 0; i < lines.size(); i++) {
+      GETPOST.add(lines.get(i).split("   ")[2]);
+      if (GETPOST.get(i).contains("GET")) {
+        GET++;
+      }
+      if (GETPOST.get(i).contains("POST")) {
+        POST++;
+      }
+    }
+    double ratio = GET / POST;
+    return ratio;
+  }
 }
