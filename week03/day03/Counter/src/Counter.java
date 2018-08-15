67ae390066b2f2dcbc92485a88032720c3f674ca
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-15 17:37:10 +0200

    Counter works...

--- week03/day03/Counter/src/Counter.java
+++ week03/day03/Counter/src/Counter.java
@@ -1,2 +1,29 @@
 public class Counter {
+  int counter = 0;
+  int init;
+
+  public Counter() {
+  }
+
+  public Counter(int init) {
+    this.init = init;
+    counter = init;
+  }
+
+  public void add() {
+    counter++;
+  }
+
+  public void add(int number) {
+    counter += number;
+  }
+
+  public int get() {
+    return counter;
+  }
+
+  public int reset() {
+    this.counter = init;
+    return counter;
+  }
 }
