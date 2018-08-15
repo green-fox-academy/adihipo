0be871fe47ca5065abef83289150e775ff5bc937
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-15 12:19:16 +0200

    Copied code to Dominoes and Main

--- week03/day03/Dominoes/src/Dominoes.java
+++ week03/day03/Dominoes/src/Dominoes.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+
 public class Dominoes {
-  
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
 }
