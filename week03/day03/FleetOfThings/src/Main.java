2608ea6af9a995036e5e8e80db7b96eb7f9cc954
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-15 16:17:05 +0200

    FleetOfThings done

--- week03/day03/FleetOfThings/src/Main.java
+++ week03/day03/FleetOfThings/src/Main.java
@@ -2,6 +2,23 @@ public class Main {
 
   public static void main(String[] args) {
     Fleet fleet = new Fleet();
+
+    Things first = new Things("Get milk");
+    fleet.add(first);
+
+    Things second = new Things("Remove the obstacles");
+    fleet.add(second);
+
+    Things third = new Things("Stand up");
+    fleet.add(third);
+    third.complete();
+
+    Things fourth = new Things("Eat lunch");
+    fleet.add(fourth);
+    fourth.complete();
+
+
+
     // Create a fleet of things to have this output:
     // 1. [ ] Get milk
     // 2. [ ] Remove the obstacles
