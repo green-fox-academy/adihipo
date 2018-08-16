3ed750ff90fc90ffc55bf4f237674b1d4735aa21
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-16 10:26:14 +0200

    pirates on ship

--- week03/day04/Pirates/src/BattleApp.java
+++ week03/day04/Pirates/src/BattleApp.java
@@ -1,21 +1,27 @@
+import java.util.ArrayList;
+
 public class BattleApp {
   public static void main(String[] args) {
-    Pirate Parrot = new Pirate();
-    Parrot.rumDrunken = 3;
-    System.out.println(Parrot.rumDrunken);
-    Parrot.howsItGoingMate();
-    Parrot.drinkSomeRum(2);
-    System.out.println(Parrot.rumDrunken);
-    Parrot.howsItGoingMate();
-    Parrot.drinkSomeRum(2);
-
-    Pirate Captain = new Pirate();
-    Captain.rumDrunken = 3;
-    System.out.println(Captain.luck);
-    System.out.println(Parrot.luck);
-    Captain.brawl(Parrot);
-    System.out.println(Captain.passedOut);
-    System.out.println(Parrot.passedOut);
+//    Pirate Parrot = new Pirate();
+//    Parrot.rumDrunken = 3;
+//    System.out.println(Parrot.rumDrunken);
+//    Parrot.howsItGoingMate();
+//    Parrot.drinkSomeRum(2);
+//    System.out.println(Parrot.rumDrunken);
+//    Parrot.howsItGoingMate();
+//    Parrot.drinkSomeRum(2);
+//
+//    Pirate Captain = new Pirate();
+//    Captain.rumDrunken = 3;
+//    System.out.println(Captain.luck);
+//    System.out.println(Parrot.luck);
+//    Captain.brawl(Parrot);
+//    System.out.println(Captain.passedOut);
+//    System.out.println(Parrot.passedOut);
+//
+    Ship ship1 = new Ship();
+    ship1.fillShip();
+    ship1.soutShip();
 
   }
 }
--- week03/day04/Pirates/src/Ship.java
+++ week03/day04/Pirates/src/Ship.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Ship {
+  ArrayList<Pirate> ship;
+  Pirate pirate = new Pirate();
+
+  public Ship() {
+    ship = new ArrayList<>();
+  }
+
+  public void fillShip() {
+    for (int i = 0; i < 10 + (int) (Math.random() * 20); i++) {
+      ship.add(pirate);
+    }
+  }
+
+  public void soutShip() {
+    System.out.println("Captain rums: " + ship.get(0).rumDrunken + "\nCaptain passed out?  " + ship.get(0).passedOut + "\nCrew size: " + (ship.size() - 1));
+    System.out.println(ship);
+  }
+
+
 }
