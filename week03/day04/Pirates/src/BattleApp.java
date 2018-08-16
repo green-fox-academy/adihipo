24f2f07a83a9829aa18c6f633b8c43311bbabd3b
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-16 08:38:01 +0200

    Pirate Class almost finished

--- week03/day04/Pirates/src/BattleApp.java
+++ week03/day04/Pirates/src/BattleApp.java
@@ -1,5 +1,20 @@
 public class BattleApp {
   public static void main(String[] args) {
-    
+    Pirate Parrot = new Pirate("Gutten Morgen");
+    Parrot.rumDrunken = 3;
+    System.out.println(Parrot.rumDrunken);
+    Parrot.howsItGoingMate();
+    Parrot.drinkSomeRum(2);
+    System.out.println(Parrot.rumDrunken);
+    Parrot.howsItGoingMate();
+    System.out.println(Parrot.luck);
+    Parrot.drinkSomeRum(2);
+
+    Pirate Captain = new Pirate("White Beard");
+    Captain.rumDrunken = 3;
+    System.out.println(Captain.luck);
+    Captain.brawl("Gutten Morgen");
+    System.out.println(Captain.passedOut);
+    System.out.println(Parrot.passedOut);
   }
 }
--- week03/day04/Pirates/src/Pirate.java
+++ week03/day04/Pirates/src/Pirate.java
@@ -1,2 +1,40 @@
 public class Pirate {
+  String name;
+  int rumDrunken = 0;
+  boolean passedOut = false;
+  double luck = Math.random();
+
+  public Pirate(String name) {
+    this.name = name;
+  }
+
+  public void drinkSomeRum(int numberOfRounds) {
+    if (passedOut) {
+      System.out.println("He's dead");
+    } else {
+      rumDrunken += numberOfRounds;
+    }
+  }
+
+  public void howsItGoingMate() {
+    if (passedOut) {
+      System.out.println("He's dead");
+    } else {
+      if (rumDrunken <= 4) {
+        System.out.println("Pour me anudder!");
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      }
+    }
+  }
+
+  public void die() {
+    passedOut = true;
+  }
+
+  public void brawl(String name) {
+    new Pirate(name).luck <= 1 / 3){
+      Pirate(name).passedOut = true;
+    }
+  }
 }
