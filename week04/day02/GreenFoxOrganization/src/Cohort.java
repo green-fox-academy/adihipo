eb8ad70dff91253e2b61d0257404fee84d2ffb76
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-21 10:31:31 +0200

    Added Cohort, almost done

--- week04/day02/GreenFoxOrganization/src/Cohort.java
+++ week04/day02/GreenFoxOrganization/src/Cohort.java
@@ -1,4 +1,26 @@
-public class Cohort {
-  
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cohort {
+  String name;
+  List<Student> students;
+  List<Mentor> mentors;
+
+  public Cohort(String name) {
+    this.name = name;
+    students = new ArrayList<Student>();
+    mentors = new ArrayList<Mentor>();
+  }
+
+  void addStudent() {
+
+  }
+
+  void addMentor() {
+
+  }
+
+  void info() {
+    System.out.println("The " + name + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
+  }
 }
