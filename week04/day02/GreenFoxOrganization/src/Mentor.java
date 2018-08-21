8f72758a1a5d8cfa3cf7cd60187e5ac5d231e5d1
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-21 10:19:04 +0200

    Inheritance in GFO

--- week04/day02/GreenFoxOrganization/src/Mentor.java
+++ week04/day02/GreenFoxOrganization/src/Mentor.java
@@ -1,2 +1,20 @@
-public class Mentor {
+public class Mentor extends Person {
+  String level;
+
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  public Mentor() {
+    level = "intermediate";
+  }
+
+  void introduce() {
+    System.out.println("Hi, I am " + name + " a " + age + " year old " + gender + " " + level + " mentor.");
+  }
+
+  void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
 }
--- week04/day02/GreenFoxOrganization/src/Sponsor.java
+++ week04/day02/GreenFoxOrganization/src/Sponsor.java
@@ -1,2 +1,27 @@
-public class Sponsor {
+public class Sponsor extends Person {
+  String company;
+  int hiredStudents;
+
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    company = "Google";
+    hiredStudents = 0;
+  }
+
+  void hire() {
+    hiredStudents++;
+  }
+
+  void introduce() {
+    System.out.println("Hi, I am " + name + " a " + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
 }
