74181f2f3b78b6a9f43ac257b939fcb2ac57931d
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-22 13:47:43 +0200

    Init CharSequence

---
+++ week04/day03/CharSequence/src/com/company/Gnirts.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.stream.IntStream;
+
+public class Gnirts implements CharSequence {
+
+  String string;
+
+  public Gnirts(String string) {
+    this.string = string;
+  }
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public char charAt(int index) {
+    return 0;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return null;
+  }
+
+  @Override
+  public IntStream chars() {
+    return null;
+  }
+
+  @Override
+  public IntStream codePoints() {
+    return null;
+  }
+}
