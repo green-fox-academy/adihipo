78635d9096a1ef6be6aa0d3a64f3334145967141
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-22 14:59:47 +0200

    CharSequence hooraay working

--- week04/day03/CharSequence/src/com/company/Shifter.java
+++ week04/day03/CharSequence/src/com/company/Shifter.java
@@ -13,16 +13,20 @@ public class Shifter implements CharSequence {
 
   @Override
   public int length() {
-    return this.length();
+    return this.string.length();
   }
 
   @Override
   public char charAt(int index) {
-    return 0;
+    return this.string.charAt(index + this.number);
   }
 
   @Override
   public CharSequence subSequence(int start, int end) {
-    return null;
+    String subseq = "";
+    for (; end >= start; start++) {
+      subseq += this.string.charAt(start + this.number);
+    }
+    return subseq;
   }
 }
