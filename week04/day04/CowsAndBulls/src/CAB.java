8bf0c6120dc1dfbd275e9c3375a14b310a8f69c7
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-23 19:14:42 +0200

    CAB working. Love it

--- week04/day04/CowsAndBulls/src/CAB.java
+++ week04/day04/CowsAndBulls/src/CAB.java
@@ -2,20 +2,56 @@ import java.util.Scanner;
 
 public class CAB {
 
-  int[] numberToGuess = {1, 2, 3, 4};
-  int[] yourGuess = new int[4];
-  String state = "playing";
+  String numberToGuess = "1234";
+  String yourGuess = "";
+  String cowsnbulls = "";
   int numberOfGuesses = 0;
 
 
-  public String cowsAndBulls() {
+  public void cowsAndBulls() {
+    reset();
+    yourGuess = getYourGuess();
+    cowsnbulls = getCowsnBulls();
+    status();
+  }
+
+  public void reset() {
+    cowsnbulls = "";
+    yourGuess = "";
+  }
+
+  public String getYourGuess() {
     for (int i = 0; i < 4; i++) {
       System.out.println("Please type your " + (i + 1) + ". number");
       Scanner scanner = new Scanner(System.in);
-      yourGuess[i] = scanner.nextInt();
+      yourGuess += scanner.nextInt();
     }
+    return yourGuess;
+  }
+
+  public String getCowsnBulls() {
+    for (int i = 0; i < 4; i++) {
+      if (numberToGuess.charAt(i) == yourGuess.charAt(i)) {
+        cowsnbulls += "bull ";
+      } else if (numberToGuess.contains(yourGuess.substring(i, i + 1))) {
+        cowsnbulls += "cow ";
+      } else cowsnbulls += yourGuess.charAt(i) + " ";
+    }
+    return cowsnbulls;
+  }
+
+  public void status() {
     System.out.println("Your guess is:");
-    System.out.println(yourGuess.toString());
-    return yourGuess.toString();
+    System.out.println(yourGuess);
+    System.out.println(cowsnbulls);
+    numberOfGuesses++;
+    System.out.println("Number of your guesses so far: " + numberOfGuesses);
+    if (numberToGuess.equals(yourGuess)) {
+      System.out.println("You won!");
+    } else {
+      System.out.println("You lose, try again!");
+      System.out.println();
+      cowsAndBulls();
+    }
   }
 }
