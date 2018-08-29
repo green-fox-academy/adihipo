832990654d08eec3a9de21dd644c21f3b73c3185
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-29 15:23:47 +0200

    getMostProductiveYear method now prints out years as List

--- week05/TestTest/src/MostProductiveYear.java
+++ week05/TestTest/src/MostProductiveYear.java
@@ -2,11 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MostProductiveYear {
   public static void main(String[] args) {
-    String filename = "marvel.txt";
+    String filename = "src/paramount.txt";
     getMostProductiveYear(filename);
   }
 
@@ -14,6 +15,13 @@ public class MostProductiveYear {
     Path path = Paths.get(filename);
     try {
       List<String> lines = Files.readAllLines(path);
+      List<String> years = new ArrayList<>();
+      int counter = 0;
+      for (int i = 0; i < lines.size(); i++) {
+        String movieDate = lines.get(i).split(",")[1];
+        years.add(movieDate);
+      }
+      System.out.println(years);
     } catch (IOException ex) {
       System.out.println("Unable to open file, sorry master!");
     }
