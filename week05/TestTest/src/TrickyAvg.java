dcbff7ddc5bf38bf67e1894e0ff301a228a0efa0
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-29 14:41:07 +0200

    Init TrickyAvg

---
+++ week05/TestTest/src/TrickyAvg.java
@@ -0,0 +1,13 @@
+public class TrickyAvg {
+
+  public static void main(String[] args) {
+    int[] numberArray = {1, 2, 3};
+    System.out.println(getTrickyAvg(numberArray));
+
+  }
+
+  public static int getTrickyAvg(int[] numberArray) {
+    int avg = 0
+    return avg;
+  }
+}
