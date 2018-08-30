39860881c04d68e5082edef4d9681103cef5d57f
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-30 15:27:13 +0200

    Logs done with regular expressions

--- week05/TestTest/src/LogsLikeBen.java
+++ week05/TestTest/src/LogsLikeBen.java
@@ -9,26 +9,23 @@ import java.util.regex.Pattern;
 
 public class LogsLikeBen {
   public static void main(String[] args) {
-    String filename = "log.txt";
-    getIPs(filename);
+    String filename = "src/log.txt";
+    System.out.println(getIPs(filename));
   }
 
   public static ArrayList<String> getIPs(String filename) {
     ArrayList<String> IPs = new ArrayList<>();
     Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}\\.\\d{2}\\.\\d{2}");
-    Matcher matcher = pattern.matcher(filename);
     Path getPath = Paths.get(filename);
     try {
       List<String> lines = Files.readAllLines(getPath);
+      Matcher matcher = pattern.matcher(lines.toString());
+      while (matcher.find()) {
+        IPs.add(matcher.group());
+      }
     } catch (IOException ex) {
       System.out.println("Unable to read file: " + filename);
     }
-
-    while (matcher.find()) {
-      System.out.println(matcher.group());
-    }
-
-
     return IPs;
   }
 }
