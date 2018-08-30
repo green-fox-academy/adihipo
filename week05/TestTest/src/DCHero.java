9b9f8bd69e56eb4466954a4cc13fdb5535373e4c
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-08-30 10:19:40 +0200

    Init classes, done Hero class

---
+++ week05/TestTest/src/DCHero.java
@@ -0,0 +1,2 @@
+public class DCHero {
+}
---
+++ week05/TestTest/src/Hero.java
@@ -0,0 +1,40 @@
+public class Hero extends BaseHero implements Punchable {
+  int motivation;
+
+  Hero(String name, int motivation) {
+    super(name);
+    this.motivation = motivation;
+  }
+
+  @Override
+  public void punch(Punchable other) {
+    if (this.getMotivationLevel() > 0) {
+      double damage = this.motivation / 1.5;
+      other.bePunched(damage);
+    }
+  }
+
+  @Override
+  public int getMotivationLevel() {
+    if (this.motivation > 40) {
+      return 2;
+    } else if (this.motivation <= 40 && this.motivation >= 20) {
+      return 1;
+    } else
+      return 0;
+  }
+
+  @Override
+  public void bePunched(double damage) {
+    this.motivation -= damage / this.motivation;
+  }
+
+  public void toStringHero() {
+    if (this.getMotivationLevel() == 2) {
+      System.out.println(this.getName() + " is well motivated");
+    } else if (this.getMotivationLevel() == 1) {
+      System.out.println(this.getName() + " is motivated");
+    } else
+      System.out.println(this.getName() + " is not motivated anymore");
+  }
+}
