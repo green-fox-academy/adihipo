cb4aff7f9321f498e08342de53e64837441cdf90
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-10 14:41:48 +0200

    Init Exercise10 and Fox class

---
+++ week06/day01/src/Exercise10.java
@@ -0,0 +1,11 @@
+public class Exercise10 {
+  public static void main(String[] args) {
+
+    Fox fox1 = new Fox("Dave", "Pallida", "greyish red");
+    Fox fox2 = new Fox("C", "Alopex", "white");
+    Fox fox3 = new Fox("Adam", "Red", "red and white");
+    Fox fox4 = new Fox("Chippy", "Desert", "creme");
+    Fox fox5 = new Fox("Kriszti", "Pallida", "greyish red");
+
+  }
+}
