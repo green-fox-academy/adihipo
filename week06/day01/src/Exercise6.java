aef4bc4aae6453fba93a9796260c2a50b2853716
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-10 13:19:55 +0200

    Exercise6 done

--- week06/day01/src/Exercise6.java
+++ week06/day01/src/Exercise6.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise6 {
   public static void main(String[] args) {
 
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
 
+    List<String> strartsAendsI = cities.stream()
+            .filter(s -> s.startsWith("A"))
+            .filter(s -> s.endsWith("I"))
+            .collect(Collectors.toList());
 
+    System.out.println(strartsAendsI);
   }
 }
