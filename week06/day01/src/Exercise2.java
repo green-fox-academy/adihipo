3da357f82ccc8186921fb5425bae5d9551bc5d01
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-10 11:19:00 +0200

    Exercise2 unnecessary imports deleted & intstream included in comment

--- week06/day01/src/Exercise2.java
+++ week06/day01/src/Exercise2.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.OptionalDouble;
-import java.util.stream.Collectors;
 
 public class Exercise2 {
   public static void main(String[] args) {
