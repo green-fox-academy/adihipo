b8d3e978418d950275eb19596377004f0b9c8423
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-10 11:25:57 +0200

    Exercise4 Init

---
+++ week06/day01/src/Exercise4.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+  }
+}
