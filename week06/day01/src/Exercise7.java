f22d4fc1af943ab803a7c64f9ffb59d80ec40c8e
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-10 13:42:50 +0200

    Exercise7 works in a way...

--- week06/day01/src/Exercise7.java
+++ week06/day01/src/Exercise7.java
@@ -1,9 +1,16 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Exercise7 {
   public static void main(String[] args) {
 
     String string = "How many times a character occurs?";
 
 
-    
+    System.out.println(string.chars()
+            .mapToObj(c -> Character.toString((char) c))
+            .collect(Collectors.groupingBy(x -> x, Collectors.counting())));
   }
 }
