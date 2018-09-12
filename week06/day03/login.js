86f0224c644218fe3429977a1861b9312fcf35e7
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-12 15:32:59 +0200

    Added show/hide function to login with js

---
+++ week06/day03/login.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+      x.type = "text";
+  } else {
+      x.type = "password";
+  }
+}
