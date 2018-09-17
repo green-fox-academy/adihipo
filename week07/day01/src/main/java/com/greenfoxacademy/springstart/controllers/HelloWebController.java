0abb44166ebdf8184aa1b0eb253792f9b1ef2d13
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-17 15:19:13 +0200

    Web greet + counter added

--- week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,14 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+  private static AtomicLong thisManyTimesOpenedPage = new AtomicLong(1);
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " ÁDÁM");
+  public String greeting(Model model, @RequestParam(name = "name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("thisManyTimesOpenedPage", thisManyTimesOpenedPage.getAndIncrement());
     return "greeting";
   }
-
 }
