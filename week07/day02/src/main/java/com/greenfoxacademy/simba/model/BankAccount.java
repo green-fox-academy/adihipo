107aecfb9c349425025cc6f33a5b976a9c5bb89c
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-18 14:54:33 +0200

    Id added to table

--- week07/day02/src/main/java/com/greenfoxacademy/simba/model/BankAccount.java
+++ week07/day02/src/main/java/com/greenfoxacademy/simba/model/BankAccount.java
@@ -1,15 +1,20 @@
 package com.greenfoxacademy.simba.model;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class BankAccount {
 
   private String name;
   private int balance;
   private String animalType;
+  private Long id;
+  private static AtomicLong incrementedId = new AtomicLong(1);
 
   public BankAccount(String name, int balance, String animalType) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+    this.id = incrementedId.getAndIncrement();
   }
 
   public String getName() {
@@ -34,5 +39,13 @@ public class BankAccount {
 
   public void setAnimalType(String animalType) {
     this.animalType = animalType;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 }
