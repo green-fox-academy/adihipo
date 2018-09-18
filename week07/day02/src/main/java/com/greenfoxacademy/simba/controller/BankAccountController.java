2f5bafe49f05c6af0f3d7f819444680e352c2732
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-18 16:10:54 +0200

    isItKing and isItGoodGuy added plus style

--- week07/day02/src/main/java/com/greenfoxacademy/simba/controller/BankAccountController.java
+++ week07/day02/src/main/java/com/greenfoxacademy/simba/controller/BankAccountController.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
 public class BankAccountController {
 
@@ -20,7 +16,7 @@ public class BankAccountController {
 
   @GetMapping("/account")
   public String show(Model model) {
-    BankAccount bankAccount1 = new BankAccount("Simba", 2000, "lion");
+    BankAccount bankAccount1 = new BankAccount("Simba", 2000, "lion", true, true);
     model.addAttribute("name", bankAccount1.getName());
     model.addAttribute("balance", bankAccount1.getBalance());
     model.addAttribute("animalType", bankAccount1.getAnimalType());
@@ -30,18 +26,20 @@ public class BankAccountController {
   @GetMapping("/list/accounts")
   public String showAll(Model model) {
     ListOfBankAccounts listOfBankAccounts = new ListOfBankAccounts();
-    BankAccount bankAccount1 = new BankAccount("Simba", 2000, "african lion");
-    BankAccount bankAccount2 = new BankAccount("Nala", 10000, "african alion");
-    BankAccount bankAccount3 = new BankAccount("Zazu", 70, "red-billed hornbill");
-    BankAccount bankAccount4 = new BankAccount("Timon", 115, "meerkat");
-    BankAccount bankAccount5 = new BankAccount("Pumbaa", 210, "common warthog");
-    BankAccount bankAccount6 = new BankAccount("Rafiki", 20000, "mandrill");
+    BankAccount bankAccount1 = new BankAccount("Simba", 2000, "african lion", true, true);
+    BankAccount bankAccount2 = new BankAccount("Nala", 10000, "african alion", false, true);
+    BankAccount bankAccount3 = new BankAccount("Zazu", 70, "red-billed hornbill", false, true);
+    BankAccount bankAccount4 = new BankAccount("Zira", 7000, "african lion", true, false);
+    BankAccount bankAccount5 = new BankAccount("Timon", 115, "meerkat", false, true);
+    BankAccount bankAccount6 = new BankAccount("Pumbaa", 210, "common warthog", false, true);
+    BankAccount bankAccount7 = new BankAccount("Rafiki", 20000, "mandrill", false, true);
     listOfBankAccounts.add(bankAccount1);
     listOfBankAccounts.add(bankAccount2);
     listOfBankAccounts.add(bankAccount3);
     listOfBankAccounts.add(bankAccount4);
     listOfBankAccounts.add(bankAccount5);
     listOfBankAccounts.add(bankAccount6);
+    listOfBankAccounts.add(bankAccount7);
 
     model.addAttribute("listOfBankAccounts", listOfBankAccounts.getBankAccountList());
     return "listofbankaccounts";
--- week07/day02/src/main/java/com/greenfoxacademy/simba/model/BankAccount.java
+++ week07/day02/src/main/java/com/greenfoxacademy/simba/model/BankAccount.java
@@ -9,12 +9,16 @@ public class BankAccount {
   private String animalType;
   private Long id;
   private static AtomicLong incrementedId = new AtomicLong(1);
+  private boolean isItKing;
+  private boolean isItGoodGuy;
 
-  public BankAccount(String name, int balance, String animalType) {
+  public BankAccount(String name, int balance, String animalType, boolean isItKing, boolean isItGoodGuy) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
     this.id = incrementedId.getAndIncrement();
+    this.isItKing = isItKing;
+    this.isItGoodGuy = isItGoodGuy;
   }
 
   public String getName() {
@@ -48,4 +52,21 @@ public class BankAccount {
   public void setId(Long id) {
     this.id = id;
   }
+
+  public boolean isItKing() {
+    return isItKing;
+  }
+
+  public void setItKing(boolean itKing) {
+    isItKing = itKing;
+  }
+
+  public boolean isItGoodGuy() {
+    return isItGoodGuy;
+  }
+
+  public void setItGoodGuy(boolean itGoodGuy) {
+    isItGoodGuy = itGoodGuy;
+  }
+
 }
