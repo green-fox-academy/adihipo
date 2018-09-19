d8226b298a3654ca4deed0588fb2d773f26576c7
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-19 16:45:19 +0200

    Added StudentService too

--- week07/day03/src/main/java/com/week07day03/practice/Controller/GFAController.java
+++ week07/day03/src/main/java/com/week07day03/practice/Controller/GFAController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GFAController {
-  
+
   @GetMapping("/gfa")
   public String showGFAhome() {
     return "gfahome";
