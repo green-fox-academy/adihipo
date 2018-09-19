caaa3cbe557702118babaa78141abd0987b2669e
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-19 14:27:20 +0200

    Random colored page done

--- week07/day03/src/main/java/com/week07day03/practice/Controller/UtilityController.java
+++ week07/day03/src/main/java/com/week07day03/practice/Controller/UtilityController.java
@@ -1,10 +1,20 @@
 package com.week07day03.practice.Controller;
 
+import com.week07day03.practice.Service.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UtilityController {
+
+  UtilityService utilityService;
+
+  @Autowired
+  public UtilityController(UtilityService utilityService) {
+    this.utilityService = utilityService;
+  }
 
   @GetMapping("/useful")
   public String showUsefulUtilities() {
@@ -12,7 +22,8 @@ public class UtilityController {
   }
 
   @GetMapping("/useful/colored")
-  public String showColoredBackground(){
+  public String showColoredBackground(Model model) {
+    model.addAttribute("backgroundColor", utilityService.randomColor());
     return "colored";
   }
 }
