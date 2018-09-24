f238cbe331aab61b19c30c896dbe243c0c7403e7
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-24 22:34:01 +0200

    Basics of the login and newgame are done

--- week08/ChessApp/src/main/java/com/greenfox/chess/Controller/MainController.java
+++ week08/ChessApp/src/main/java/com/greenfox/chess/Controller/MainController.java
@@ -2,6 +2,7 @@ package com.greenfox.chess.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -13,7 +14,7 @@ public class MainController {
   }
 
   @PostMapping("/")
-  public String getPlayerInfo() {
+  public String getPlayerInfo(@ModelAttribute(value = "whiteName") String whiteName, @ModelAttribute(value = "blackName") String blackName) {
     return "redirect:/newgame";
   }
 
