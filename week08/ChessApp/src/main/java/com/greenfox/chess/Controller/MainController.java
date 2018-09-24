e3fd4cfba653ecf2c494251e1ee34f0009116ac6
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-24 21:08:02 +0200

    Navigation possible

--- week08/ChessApp/src/main/java/com/greenfox/chess/Controller/MainController.java
+++ week08/ChessApp/src/main/java/com/greenfox/chess/Controller/MainController.java
@@ -1,10 +1,20 @@
 package com.greenfox.chess.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
+  @GetMapping("/")
+  public String showMainPage() {
+    return "login";
+  }
 
+  @PostMapping("/")
+  public String getPlayerInfo() {
+    return "redirect:/newgame";
+  }
 
 }
