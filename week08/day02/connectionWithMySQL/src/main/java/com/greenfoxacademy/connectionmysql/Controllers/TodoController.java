ac0838bd505505fd60d44b03500363bfc5b89d73
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-25 16:02:13 +0200

    Add new todo available

--- week08/day02/connectionWithMySQL/src/main/java/com/greenfoxacademy/connectionmysql/Controllers/TodoController.java
+++ week08/day02/connectionWithMySQL/src/main/java/com/greenfoxacademy/connectionmysql/Controllers/TodoController.java
@@ -1,10 +1,13 @@
 package com.greenfoxacademy.connectionmysql.Controllers;
 
+import com.greenfoxacademy.connectionmysql.Models.Todo;
 import com.greenfoxacademy.connectionmysql.Reporitories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,6 +18,17 @@ public class TodoController {
   @Autowired
   public TodoController(TodoRepository todoRepository) {
     this.todoRepository = todoRepository;
+  }
+
+  @GetMapping("/todo")
+  public String showTodoForm() {
+    return "todo";
+  }
+
+  @PostMapping("/todo")
+  public String addTodo(@ModelAttribute(value = "title") String title) {
+    todoRepository.save(new Todo(title));
+    return "redirect:/list";
   }
 
   @GetMapping(value = {"/", "/list"})
