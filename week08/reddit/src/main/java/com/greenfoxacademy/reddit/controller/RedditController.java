6cf92848447d57a25f28e2d211885952fe55842f
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-09-28 10:31:08 +0200

    endpoints added

--- week08/reddit/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
+++ week08/reddit/src/main/java/com/greenfoxacademy/reddit/controller/RedditController.java
@@ -1,17 +1,35 @@
 package com.greenfoxacademy.reddit.controller;
 
+import com.greenfoxacademy.reddit.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RedditController {
 
+  private PostService postService;
+
+  @Autowired
+  public RedditController(PostService postService) {
+    this.postService = postService;
+  }
 
   @GetMapping("/")
   public String showMain() {
+    postService.getAllPosts();
     return "main";
   }
 
-  
+  @GetMapping("/submit")
+  public String showSubmit() {
+    return "submit";
+  }
+
+  @PostMapping("/submit")
+  public String postSubmit(){
+    return"redirect:";
+  }
 
 }
--- week08/reddit/src/main/java/com/greenfoxacademy/reddit/service/PostServiceImpl.java
+++ week08/reddit/src/main/java/com/greenfoxacademy/reddit/service/PostServiceImpl.java
@@ -3,15 +3,22 @@ package com.greenfoxacademy.reddit.service;
 import com.greenfoxacademy.reddit.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 @Service
-public class PostServiceImpl implements PostService{
+public class PostServiceImpl implements PostService {
 
   private PostRepository postRepository;
+  private Model model;
 
   @Autowired
   public PostServiceImpl(PostRepository postRepository) {
     this.postRepository = postRepository;
   }
+
+  public void getAllPosts() {
+    model.addAttribute(postRepository.findAll());
+  }
+
 
 }
