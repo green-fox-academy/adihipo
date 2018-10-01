9d5958845efb1e0f3744e54e4079b92afe21f1a8
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-10-01 12:55:07 +0200

    chore(exerciceTester)

---
+++ week09/day01/ExerciceTesterPlusExercices/src/main/java/com/greenfoxacademy/exercices/ExercicesApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.exercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExercicesApplication.class, args);
+  }
+}
