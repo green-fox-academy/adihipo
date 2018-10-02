6f4e4e023dcb321155cc4b35f5bf4b4fb4b95488
Gyarmati ADAM <gyarmatiaadaam@gmail.com>
2018-10-02 14:39:37 +0200

    test(yondu) init

--- week09/day02/Yondu/src/test/java/com/greenfoxacademy/yondu/YonduApplicationTests.java
+++ week09/day02/Yondu/src/test/java/com/greenfoxacademy/yondu/YonduApplicationTests.java
@@ -1,16 +1,40 @@
 package com.greenfoxacademy.yondu;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfoxacademy.yondu.controller.ArrowController;
+import com.greenfoxacademy.yondu.service.ArrowService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(ArrowController.class)
 public class YonduApplicationTests {
 
-  @Test
-  public void contextLoads() {
-  }
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private ArrowService arrowService;
+
+
 
 }
