4851496328da5644fcbb608c72674194b4a60760
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2018-10-11 15:36:37 +0200

    test(bucketList): fixed some small issues

--- week10/Authentication/src/test/java/com/greenfoxacademy/authentication/AuthenticationApplicationTests.java
+++ week10/Authentication/src/test/java/com/greenfoxacademy/authentication/AuthenticationApplicationTests.java
@@ -3,42 +3,32 @@ package com.greenfoxacademy.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfoxacademy.authentication.contollers.BucketListRESTController;
 import com.greenfoxacademy.authentication.models.Goal;
-import com.greenfoxacademy.authentication.security.AccountCredentials;
+import com.greenfoxacademy.authentication.repositories.GoalRepository;
 import com.greenfoxacademy.authentication.services.BucketListService;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(BucketListRESTController.class)
+@WebMvcTest({BucketListRESTController.class, GoalRepository.class})
 public class AuthenticationApplicationTests {
 
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
@@ -47,6 +37,9 @@ public class AuthenticationApplicationTests {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @Mock
+  private GoalRepository goalRepository;
 
   @MockBean
   private BucketListService bucketListService;
@@ -68,9 +61,11 @@ public class AuthenticationApplicationTests {
     Boolean done = true;
 
     Goal newGoal = new Goal(text, dreamLevel, done);
+    goalRepository.save(newGoal);
     List<Goal> goals = new ArrayList<>();
     goals.add(newGoal);
 
+    List<Goal> goals2 = goalRepository.findAll();
     when(bucketListService.getAllGoalsAsList()).thenReturn(goals);
 
     mockMvc.perform(MockMvcRequestBuilders.get("/api/list")
@@ -79,12 +74,13 @@ public class AuthenticationApplicationTests {
             .andExpect(content().contentType(contentType))
             .andExpect(jsonPath("$[0].text", is(text)))
             .andExpect(jsonPath("$[0].dreamLevel", is(dreamLevel)))
-            .andExpect(jsonPath("$[0].done", is(done)));
+            .andExpect(jsonPath("$[0].done", is(done)))
+            .andDo(print());
   }
 
   @Test
   @WithMockUser(username = "admin", password = "{noop}password", roles = "ADMIN")
-  public void givenAdminUser_whenSaveNewGoal_thenOk() throws Exception {
+  public void givenAdminUser_whenSaveNewGoal_thenOkAndData() throws Exception {
 
     String text = "Finish authentication";
     Integer dreamLevel = 100;
@@ -95,9 +91,9 @@ public class AuthenticationApplicationTests {
     String newGoalAsString = objectMapper.writeValueAsString(newGoal);
 
     mockMvc.perform(MockMvcRequestBuilders.post("/api/save")
-            .contentType(MediaType.ALL)
+            .contentType(contentType)
             .content(newGoalAsString)
-            .accept(MediaType.ALL))
+            .accept(contentType))
             .andExpect(status().isOk())
             .andExpect(content().contentType(contentType))
             .andExpect(jsonPath("$.text", is(text)))
