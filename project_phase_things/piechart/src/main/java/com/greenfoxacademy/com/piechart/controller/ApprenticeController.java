e630d7d366919a3198d0b08ff32c0d0170ca1144
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2018-11-07 13:27:25 +0100

    feat(ApprenticeController): simple endpoint for index

--- project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/controller/ApprenticeController.java
+++ project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/controller/ApprenticeController.java
@@ -1,4 +1,13 @@
 package com.greenfoxacademy.com.piechart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApprenticeController {
+
+  @GetMapping("/")
+  public String showPieChart(){
+    return "apprentice";
+  }
 }
