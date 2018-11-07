23e6d520e9ca24836ee1508f538b61472c68e7ee
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2018-11-07 13:23:30 +0100

    feat(apprentice model)

--- project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/model/Apprentice.java
+++ project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/model/Apprentice.java
@@ -1,4 +1,45 @@
 package com.greenfoxacademy.com.piechart.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Apprentice {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String cohort;
+
+  public Apprentice(String name, String cohort) {
+    this.name = name;
+    this.cohort = cohort;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCohort() {
+    return cohort;
+  }
+
+  public void setCohort(String cohort) {
+    this.cohort = cohort;
+  }
 }
--- project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/model/ApprenticeRepository.java
+++ project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/model/ApprenticeRepository.java
@@ -1,4 +1,9 @@
 package com.greenfoxacademy.com.piechart.model;
 
-public interface ApprenticeRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApprenticeRepository extends CrudRepository<Apprentice, Long> {
+
 }
