53579cef3b807e5b1c2654fba616e8c246167bcc
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2018-11-08 17:19:47 +0100

    feat(google piechart)

--- project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/controller/ApprenticeController.java
+++ project_phase_things/piechart/src/main/java/com/greenfoxacademy/com/piechart/controller/ApprenticeController.java
@@ -4,7 +4,12 @@ import com.greenfoxacademy.com.piechart.service.ApprenticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
@@ -19,6 +24,11 @@ public class ApprenticeController {
 
   @GetMapping("/")
   public String showPieChart(Model model){
+    List<String> technologies = Arrays.asList("Java", "Javascript", "Devops");
+    List<Integer> numbers = Arrays.asList(28, 12,7);
+    model.addAttribute("technologies", technologies);
+    model.addAttribute("numbers", numbers);
     return "apprentice";
   }
+
 }
