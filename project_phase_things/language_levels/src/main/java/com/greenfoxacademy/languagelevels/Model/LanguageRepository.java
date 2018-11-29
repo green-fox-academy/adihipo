b40027d681f171cf22e6543d7802fe5b0319810d
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2018-11-29 09:03:04 +0100

    feat(getAllLanguages): select distinct languages

--- project_phase_things/language_levels/src/main/java/com/greenfoxacademy/languagelevels/Model/LanguageRepository.java
+++ project_phase_things/language_levels/src/main/java/com/greenfoxacademy/languagelevels/Model/LanguageRepository.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.languagelevels.Model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Repository
 public interface LanguageRepository extends CrudRepository<Language, Long> {
 
+  @Query("SELECT language FROM Language language GROUP BY language.name")
   List<Language> findAll();
 }
